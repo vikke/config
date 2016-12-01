aefbc21f6d90c43085f508b691f863ea08b55ff4
MATSUNAGA Ichiro <vikke.bsd@gmail.com>
2016-12-01 17:48:22 +0900

    remove.

--- bin/import_dumps.sh
+++
@@ -1,8 +0,0 @@
-cd ~/wo_docker/wo
-id=$1
-env=$2
-
-echo "= import: $id ==========================="
-
-wo db dropdb
-wo db createdb && wo db import $id.$env
--- bin/kick_spec.sh
+++
@@ -1,5 +0,0 @@
-cd ~/wo_docker/wo
-
-wo a -env test rake spec:db:delete
-echo 'start spec'
-wo a spec ${@}
