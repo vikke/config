db9e9be53b7ea48f45d4444fbcdb04586c6247bf
MATSUNAGA Ichiro <vikke.bsd@gmail.com>
2016-12-01 17:18:17 +0900

    fix.

--- bin/denite_grep.sh
+++ bin/denite_grep.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-echo ${@}
-ag  --nopager --nocolor --nogroup $( printf '%q' "${@}" ) | sort -d
+if [ ${#} -ne 1 ]; then
+	exit 2
+fi
+search_word=${1}
+ag  --nopager --nocolor --nogroup "${search_word}"| sort -d
