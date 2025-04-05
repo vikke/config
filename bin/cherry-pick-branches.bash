4d8a90fba16328903c9254ae35edf112dbaba8e3
MATSUNAGA Ichiro <vikke.bsd@gmail.com>
2025-04-05 17:25:07 +0900

    cherry-pick script修正。

--- bin/cherry-pick-branches.bash
+++ bin/cherry-pick-branches.bash
@@ -7,10 +7,16 @@ NOW_BRANCH=$(git branch | cat - | awk -e '/^\*/{print $2}')
 for branch in ${BRANCHES[@]}; do
   echo "== ${branch} =="
   git checkout "${branch}"
-  git cherry-pick "${COMMIT_HASH}"
-
+  git pull
   if [ ${?} -ne 0 ]; then
 	  exit
+  fi
+
+  git cherry-pick "${COMMIT_HASH}"
+  if [ ${?} -ne 0 ]; then
+	  exit
+  else
+	git push
   fi
 done
 
